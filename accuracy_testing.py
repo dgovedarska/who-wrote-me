a2490fa46bcfd950bcb3f77a5704ff91a407c4e9
Dessislava <msm_dessi_lesnovo@abv.bg>
2016-07-04 10:42:20 +0300

    Final statistics and algorythms

--- accuracy_testing.py
+++ accuracy_testing.py
@@ -9,6 +9,8 @@ from nltk.classify.scikitlearn import SklearnClassifier
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 
 
 def unusualWords(text):
@@ -55,10 +57,18 @@ labeled_books = ([(fileid, fileid.split('-')[0]) for fileid in gutenberg.fileids
 #featureSetsFile.close()
 
 featuresets = pickle.load(open( "features.pickle", "rb"))
-random.shuffle(featuresets)
-train_set, test_set = featuresets[:50], featuresets[50:] 
 
-print(featuresets)
+def altElement(a):
+    return a[::2]
+
+train_set = featuresets[0:][::2]
+test_set = featuresets[1:][::2]
+
+
+#print(test_set)
+#print("--------------------------------------------------------------------")
+#print(train_set)
+
 
 MNB_classifier = SklearnClassifier(MultinomialNB())
 MNB_classifier.train(train_set)
@@ -77,5 +87,13 @@ print("Multinomial Naive Bayes Algorithm accuracy percent:", (nltk.classify.accu
 KNN_classifier = SklearnClassifier(KNeighborsClassifier())
 KNN_classifier.train(train_set)
 print("K Nearest Neighbors Algorithm accuracy percent:", (nltk.classify.accuracy(KNN_classifier, test_set))*100)
+
+LR_classifier = SklearnClassifier(LogisticRegression())
+LR_classifier.train(train_set)
+print("Logistic Regression Algorithm accuracy percent:", (nltk.classify.accuracy(LR_classifier, test_set))*100)
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(train_set)
+print("Linear SVC Algorithm accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, test_set))*100)
 
 
